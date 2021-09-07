28a63234f05da0aff17a6d8e5f393411018b36fb
acercyc <acercyc@gmail.com>
2021-09-07 18:57:13 +0900

    Make an example

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # gTecUtils
+* Import g.tec HDf5 file
+* Convert to MNE RawArray data type
+
+```python
+import gTecUtils as gu 
+
+dataset = gu.gTecDataset("Z:\RecordSession_2021.08.27_17.36.03.hdf5")
+mne_RawArray = dataset.toMNE()
+
+```
+2021/09/07 18:53
--- gTecUtils.py
+++ gTecUtils.py
@@ -6,17 +6,6 @@ import xml.etree.ElementTree as ET
 import pandas as pd
 from lxml import etree
 
-# %%
-fn = "Z:\RecordSession_2021.08.27_17.36.03.hdf5"
-f = h5py.File(fn, 'r')
-
-#%%
-dd = f.get('RawData/Samples')
-np.array(dd).shape
-
-
-#%%
-f.get('RawData/AcquisitionTaskDescription')
 
 # %%
 class gTecDataset:
@@ -24,24 +13,19 @@ class gTecDataset:
         self.filename = filename
         self.hdf5 = h5py.File(filename, 'r')
         self.info = self.parser()
-        self.data = self.info['RawData/Samples']
+        self.data = self.info['RawData/Samples'].T
+        self.ch_names = list(
+            self.info['RawData/AcquisitionTaskDescription']['ChannelProperties']['ChannelName'])
+        # self.ch_types = list(self.info['RawData/AcquisitionTaskDescription']['ChannelProperties']['ChannelType'])
+        self.ch_types = ['eeg' for x in range(32)]
+        self.sfreq = self.info['RawData/AcquisitionTaskDescription']['SamplingFrequency']
 
-    def printDataStructure(self):
-        def ps(name, obj):
-            print('{}\n    {}'.format(name, obj))
-        self.hdf5.visititems(ps)
-
-    def printDataStructure2(self):
-        def ps(name, obj):
-            print('{}\n    {}'.format(name, type(obj)))
-        self.hdf5.visititems(ps)
-    
     def parser(self):
         dataDict = {}
+
         def inner(name, obj):
             if type(obj) is h5py.Dataset:
-                print('{}\n    {}'.format(name, obj))
-                            
+
                 if 'S' in str(obj.dtype):
                     if 'xml' in str(obj[0][:20]):
                         xmlObj = etree.fromstring(obj[0])
@@ -51,18 +35,13 @@ class gTecDataset:
                 else:
                     dataDict[name] = np.array(obj)
 
-                
         self.hdf5.visititems(inner)
         return dataDict
-            
-            
 
-# g = gTecDataset(fn)
-# g.printDataStructure()
-# g.printDataStructure2()
-# g.parser()
-# g.info['RawData/AcquisitionTaskDescription']
-#%% 
+    def toMNE(self):
+        info = mne.create_info(self.ch_names, self.sfreq, self.ch_types)
+        return mne.io.RawArray(self.data, info)
+
 
 def xmlParser(xmlObj):
     xmlDict = {}
@@ -72,8 +51,6 @@ def xmlParser(xmlObj):
         if len(xmlObj) > 0:
             childrenTag = [c.tag for c in xmlObj]
 
-            print(childrenTag)
-
             if len(set(childrenTag)) > 1:
                 # if different children
                 for child in xmlObj:
@@ -82,17 +59,19 @@ def xmlParser(xmlObj):
                 # if same children
                 df = pd.read_xml(etree.tostring(xmlObj))
                 xmlDict[xmlObj.tag] = df
-                # print(df)
 
         else:
             # if no children
             # xmlDict.update({xmlObj.tag: xmlObj.text})
             xmlDict[xmlObj.tag] = xmlObj.text
-            print(xmlObj.tag)
-            print(xmlObj.text)
 
         return xmlDict
 
     xmlParser_inner(xmlObj)
     return xmlDict
 
+
+if __name__ == "__main__":
+    pass
+
+# %%
